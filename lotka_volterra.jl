62f89e96c058db6d4c4fa38c2f27893f128cdc92
jensvanderzee <40233352+jensvanderzee@users.noreply.github.com>
2023-11-20 19:38:07 +0100

    Add files via upload

---
+++ lotka_volterra.jl
@@ -0,0 +1,25 @@
+using DifferentialEquations
+using OrdinaryDiffEq, ModelingToolkit, DataDrivenDiffEq, SciMLSensitivity, DataDrivenSparse
+using Optimization, OptimizationOptimisers, OptimizationOptimJL
+
+# Standard Libraries
+using LinearAlgebra, Statistics
+
+# External Libraries
+using ComponentArrays, Lux, Zygote, Plots, StableRNGs
+gr()
+
+# Set a random seed for reproducible behaviour
+rng = StableRNG(1111)
+
+function lotka!(du, u, p, t)
+    α, β, γ, δ = p
+    du[1] = α * u[1] - β * u[2] * u[1]
+    du[2] = γ * u[1] * u[2] - δ * u[2]
+end
+1+1
+tspan = (0.0, 5.0)
+u0 = 5.0f0 * rand(rng, 2)
+p_ = [1.3, 0.9, 0.8, 1.8]
+prob = ODEProblem(lotka!, u0, tspan, p_)
+solution = solve(prob, Vern7(), abstol = 1e-12, reltol = 1e-12, saveat = 0.25)
